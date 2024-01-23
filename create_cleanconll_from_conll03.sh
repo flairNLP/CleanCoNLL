381aa5dc90743d4574ef0a55a3ef9cd4667867e8
susannaruecker <susanna.ruecker@hu-berlin.de>
2024-01-23 13:01:41 +0100

    add script root

--- create_cleanconll_from_conll03.sh
+++ create_cleanconll_from_conll03.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 URL="https://data.deepai.org/conll2003.zip"
-CONLL03_DIR="data/conll03/"
-PATCH_DIR="data/patch_files"
+CONLL03_DIR="$SCRIPT_ROOT/data/conll03/"
+PATCH_DIR="$SCRIPT_ROOT/data/patch_files"
 
 if [ ! -d "$CONLL03_DIR" ] || [ -z "$(ls -A "$CONLL03_DIR")" ]; then
 	echo "Downloading the original CoNLL-03 from $URL into $CONLL03_DIR ..."
@@ -15,7 +15,7 @@ fi
 
 echo "Now applying the patch files to the original CoNLL tokens to get the modified CleanCoNLL tokens..."
 
-TOKENS_DIR="data/tokens_updated"
+TOKENS_DIR="$SCRIPT_ROOT/data/tokens_updated"
 mkdir -p "$TOKENS_DIR"
 
 awk '{print $1}' "$CONLL03_DIR/train.txt" > "$CONLL03_DIR/train_tokens.txt"
@@ -27,8 +27,8 @@ patch "$CONLL03_DIR/valid_tokens.txt" "$PATCH_DIR/dev_tokens.patch" -o "$TOKENS_
 patch "$CONLL03_DIR/test_tokens.txt" "$PATCH_DIR/test_tokens.patch" -o "$TOKENS_DIR/test_tokens_updated.txt"
 
 echo "Now merging the updated token files with the CleanCoNLL annotations:"
-CLEANCONLL_ANNOTATIONS_DIR="data/cleanconll_annotations/"
-CLEANCONLL_DIR="data/cleanconll/"
+CLEANCONLL_ANNOTATIONS_DIR="$SCRIPT_ROOT/data/cleanconll_annotations/"
+CLEANCONLL_DIR="$SCRIPT_ROOT/data/cleanconll/"
 mkdir -p "$CLEANCONLL_DIR"
 
 cut -f 2- "$CLEANCONLL_ANNOTATIONS_DIR/cleanconll_annotations.train" | paste "$TOKENS_DIR/train_tokens_updated.txt" - | sed 's/^\t$//' > "$CLEANCONLL_DIR/cleanconll.train"
